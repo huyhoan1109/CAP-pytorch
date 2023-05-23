71d9489b039c50088eaf0b1a3a0e0e1f4217d31e
huyhoan1109 <huyhoan0901@gmail.com>
2023-05-23 16:24:41 +0700

    requirements.txt

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
+pip install -r requirements.txt
 python3 download.py --dataset voc2012
 python3 gen_meta.py --dataset voc2012 --labeled 0.2 --valid 0.1 --test 0.1
 python3 run.py --dataset voc2012 --train t --use-ema t --resume f 
